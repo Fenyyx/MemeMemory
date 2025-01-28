58d9631cdb8c78eaf25d5da95443f4c87b4e981e
Fenyyx <fenixg12@gmail.com>
2025-01-28 17:47:22 +0000

    Imagenes en el face y corregido otro error

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-const totalCards = 24 //Cartas en pantalla
+const totalCards = 14 //Cartas en pantalla
 let cards = [] //Cartas generadas
 let selectedCards = [] //Cartas seleccionadas
 let valuesUsed = [] //Controla cuantas veces se ha usado un valores
@@ -13,7 +13,7 @@ let model = `<div class="card"><div class="back"></div><div class="face"></div><
 
 function activate(event) {
 if(move < 2) { //Limita a 2 las cartas por turno
-   event.target.classList.add(`active`) //Le damos la clase activa a la carta seleccionada
+   
     if ((!selectedCards[0] || selectedCards[0] !== event.target) && !event.target.classList.contains(`active`)) {
         event.target.classList.add(`active`) //Le damos la clase activa a la carta seleccionada, y prevenimos que le de la vuelta a una carta ya activa
         selectedCards.push(event.target) //Si selectedCards está vacío o es diferente de la primera carta lo añadimos al array
@@ -23,11 +23,11 @@ if(move < 2) { //Limita a 2 las cartas por turno
             
         attempts++
         document.querySelector(`#stats`).innerHTML = attempts + ` ` + `intentos`//Avanza el contador al darle la vuelta a 2 cartas
-            if (selectedCards[0].querySelectorAll(`.face`)[0].innerHTML == selectedCards[1].querySelectorAll(`.face`)[0].innerHTML) // Comprueba que las 2 ccartas son iguales
+            if (selectedCards[0].querySelectorAll(`.face`)[0].innerHTML == selectedCards[1].querySelectorAll(`.face`)[0].innerHTML) // Comprueba que las 2 cartas son iguales
                {
                 selectedCards = []
                 move = 0
-            } // Reiniciaos las cartas seleccionadas y el contador de movimientos para seguir jugando
+            } // Reiniciamos las cartas seleccionadas y el contador de movimientos para seguir jugando
             else {
                 setTimeout(() => {
                     selectedCards[0].classList.remove(`active`)
@@ -54,14 +54,35 @@ function randomValue() {
 
 }// Genera valores aleatorios para las cartas y evita que aparezcan ás de 2 cartas iguales
 
+
+
+function getImage(imageid){
+    return `<img alt="img ${imageid}" src="images/${imageid}.jpg"></img>`
+
+}
+
+/* cardsContainer.innerHTML = getImage([i]) */
+
+
+
+
+
+
+
+
+
+
+
+
 for (let i = 0; i < totalCards; i++) {
     let div = document.createElement(`div`)
     div.innerHTML = model
     cards.push(div)
-    document.querySelector(`#game`).appendChild(cards[i]) // Añadimos la carta creada como un hijo de game
+    document.querySelector(`#game`).appendChild(cards[i]) // Añadimos la carta creada como un hijo de game      
     randomValue() 
-    cards[i].querySelectorAll(`.face`)[0].innerHTML = valuesUsed[i] //Asignamos un valor a la carta
+   /*  cards[i].querySelectorAll(`.face`)[0].innerHTML = valuesUsed[i] */ //Asignamos un valor a la carta
+    cards[i].querySelectorAll(`.face`)[0].innerHTML = getImage(valuesUsed[i]) //asignamos una imagen a la carta
     cards[i].querySelectorAll(`.card`)[0].addEventListener(`click`, activate) // Cuando haga click en la carta se dará la vuelta
+    
 } // Generador de cartas usando el modelo, que aplica un valor usando la fórmula random value y que permite dar la vuelta a las cartas clicando
 
- 
